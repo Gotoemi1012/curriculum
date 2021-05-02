8695b6bb9a57a8867ba18cc3494a1d8f7dac5b61
emi goto <karumia1012karakul@gmail.com>
2021-05-02 19:11:12 +0900

    Create Task1_3.java
    演算子提出課題

---
+++ JavaSutudy/1−3/Task1_3.java
@@ -0,0 +1,22 @@
+public class Task1_3 {
+   public static void main(String[] args) {
+       int plus = 0;
+       plus = 5 + 10;
+       int minus = 0;
+       minus = 20 - 7;
+       // multiplyに10と2を乗算して20を代入
+       int multiply = 10 * 2;
+       int remi = 20;
+       remi %=6;
+       // 変数townに有楽町を代入、変数lineに線を代入
+       String town = "有楽町";
+       String line = "線";
+       String train;
+       train = town + line;
+       System.out.println(plus);
+       System.out.println(minus);
+       System.out.println(multiply);
+       System.out.println(remi);
+       System.out.println(train);
+   }
+}
