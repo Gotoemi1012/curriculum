db22df08788780873ef94d73eb1eaa9de75e6e72
emi goto <karumia1012karakul@gmail.com>
2021-05-02 14:24:16 +0900

    Create Task1_2.java
    配列提出課題

---
+++ JavaSutudy/1-2/Task1_2.java
@@ -0,0 +1,12 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+        String[] country = { "JAPAN", "AMERICA", "KOREA" };
+        System.out.println(country.length);
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+        System.out.println(strArray[1]);
+        // int[]型の変数intArrayの宣言と初期化
+        int[] intArray = { 10, 20, 30, 40, 50, };
+        // intArrayの１の要素の20と４の要素の50を足した数70を表示。
+        System.out.println(intArray[1] + intArray[4]);
+    }
+}
